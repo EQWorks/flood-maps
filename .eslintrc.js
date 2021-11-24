9db40e05e2eb4c41942807272b884c9ce606debf
Erika Szabo <erika.szabo.geo@gmail.com>
2021-11-24 11:11:22 -0500

    init - further project setup

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+    'jest': true,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      jsx: true,
+    },
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    'react-hooks',
+    'import',
+  ],
+  'rules': {
+    'import/newline-after-import': ['error', { 'count': 2 }],
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+      { 'avoidEscape': true },
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+    'no-console': [
+      'warn',
+      { allow: ['error'] },
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'eol-last': [
+      'error',
+      'always',
+    ],
+    'no-trailing-spaces': [
+      'error',
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/display-name': 'off', // TODO: many false-positives until react eslint plugin updates
+  },
+}
