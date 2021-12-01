aed9b43a623c79383a9df2de1040126eda9ebb88
Erika Szabo <erika.szabo.geo@gmail.com>
2021-12-01 11:34:46 -0500

    constants - create file for constants, add locations

---
+++ src/constants.js
@@ -0,0 +1,30 @@
+export const locations = {
+  'Area map' : {
+    latitude: 49.068,
+    longitude: -122.15,
+  },
+  'West Abbotsford' : {
+    longitude: -122.249680,
+    latitude: 49.016993,
+  },
+  'Arnold' :{
+    longitude: -122.152839,
+    latitude: 49.009589,
+  },
+  'Central Sumas' : {
+    longitude: -122.172691,
+    latitude: 49.031554,
+  },
+  'East Sumas' : {
+    longitude: -122.125018,
+    latitude: 49.060461,
+  },
+  'Barrowtown' : {
+    longitude: -122.095234,
+    latitude: 49.103892,
+  },
+  'West Yarrow' : {
+    longitude: -122.087808,
+    latitude: 49.070356,
+  },
+}
