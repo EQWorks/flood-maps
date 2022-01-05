03aadb208aa95c5f7c5672b56718ee769b8804fa
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-01-05 17:48:02 -0500

    devops - re-intro explicit babel config to recover eslint

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(), // out-of-box support for babel is decent
+    react({
+      babel: { configFile: true },
+    }),
   ],
   build: {
     outDir: 'build', // map to CRA convention
